e1a5f81e5d0a29c2617e72f3e1933337a216a148
JayDee MBP <xadxunaid@gmail.com>
2022-01-09 14:11:12 +0500

    Laravel Vue boilerplate containerized with Laravel sail.

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        This is a home component. npm is running from local machine and not from the container.
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
