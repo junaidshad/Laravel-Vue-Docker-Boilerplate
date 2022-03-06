a5e2412258bc45add9af18dfb1d48b9943fcf17d
JayDee MBP <xadxunaid@gmail.com>
2022-03-06 13:11:33 +0500

    jwt auth issues fixed

--- routes/web.php
+++ routes/web.php
@@ -17,19 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Auth::routes();
-Route::group([
-
-    'middleware' => 'api',
-    'prefix' => 'auth'
-
-], function ($router) {
-
-    Route::post('login', 'AuthController@login');
-    Route::post('logout', 'AuthController@logout');
-    Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'AuthController@me');
-
-});
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
